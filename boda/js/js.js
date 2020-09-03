d157543441f272fad9ff6582e3c6d599c81a2801
Elias <elias@dev.digono.com>
2020-09-03 14:24:05 -0500

    g

--- boda/js/js.js
+++ boda/js/js.js
@@ -1,5 +1,12 @@
 
-
+var hombres = new Howl({
+	src: ["images/musica/hombres.mp3"],
+	volume: 0.5
+});
+var mujeres = new Howl({
+	src: ["images/musica/mujeres.mp3"],
+	volume: 0.5
+});
 gsap.from(".imgLiz", {opacity: 0, x: -1000, duration: 3})
 gsap.from(".imgElias", {opacity: 0, x: 1000, duration: 3})
 gsap.from(".misdamas, .miscaballeros, .seccionDatos", {opacity: 0, y: 1000, duration: 5})
@@ -11,4 +18,19 @@ setTimeout(function(){
 
 $( ".miscaballeros" ).click(function() { 
 	$('.modalCaballeros').css({'display':'flex'});
+	hombres.play();
 });
+$( ".misdamas" ).click(function() { 
+	$('.modalDamas').css({'display':'flex'});
+	mujeres.play();
+});
+$( ".cerrar" ).click(function() { 
+	$('.modalCaballeros').css({'display':'none'});
+	hombres.pause();
+});
+$( ".cerrard" ).click(function() { 
+	$('.modalDamas').css({'display':'none'});
+	mujeres.pause();
+});
+
+
