4851729fa9eecffacb2e7b3bc42631696c113f49
Elias <elias@dev.digono.com>
2020-10-06 00:08:01 -0500

    ff

--- webservice/js/js.js
+++ webservice/js/js.js
@@ -1,7 +1,6 @@
 
 
 $(document).ready(function() {
-
   new Vue({
     el: '#app',
     data () {
@@ -10,17 +9,37 @@ $(document).ready(function() {
       }
     },
     mounted () {
-      axios
-        .get('https://cors-anywhere.herokuapp.com/https://interfaz.cenart.gob.mx/api/video/catalogo/?id_post=28958')
-        .then(response => (this.info = response.data[0].video_videoclip))
+      $.ajax({
+        url:   'https://cors-anywhere.herokuapp.com/https://interfaz.cenart.gob.mx/api/video/catalogo/?id_post=28958',
+        type:  'GET',
+        beforeSend: function () {
+                $(".resultado").html("Procesando, espere por favor...");
+        },
+        success:  function (response) {
+          data = response[0];
+          //console.log(response[0].video_videoclip);
+          $(".resultado").css({'display':'none'});  
+
+       
+          data.video_videoclip.forEach(function(video, index) {
+            $(".for").append('<tr class="col-4"><td><img class="img_video" src='+video.imagen_video+'><p class="title_video">'+video.titulo_video+'</p><p class="desc_video">descripcion...</p></td></tr>');
+          });
+          $('#example').DataTable();
+
+        }
+      });
+    
+
     }
   })
     
 } );
 
-$(document).ready( function () {
-    $('#example').DataTable();
-} );
+
+/*axios
+        .get('https://cors-anywhere.herokuapp.com/https://interfaz.cenart.gob.mx/api/video/catalogo/?id_post=28958')
+        .then(response => (this.info = response.data[0].video_videoclip))
+        if (info) {alert('jjj');} */
 
 
 
