18741ba771067761ac6f528be94187ac2213bbaf
Elias <elias@dev.digono.com>
2020-10-05 23:16:23 -0500

    dd

---
+++ webservice/js/js.js
@@ -0,0 +1,51 @@
+
+
+$(document).ready(function() {
+
+  new Vue({
+    el: '#app',
+    data () {
+      return {
+        info: null
+      }
+    },
+    mounted () {
+      axios
+        .get('https://cors-anywhere.herokuapp.com/https://interfaz.cenart.gob.mx/api/video/catalogo/?id_post=28958')
+        .then(response => (this.info = response.data[0].video_videoclip))
+    }
+  })
+    
+} );
+
+$(document).ready( function () {
+    $('#example').DataTable();
+} );
+
+
+
+
+/*$( document ).ready(function() {
+  $.ajax({
+    url:   'https://cors-anywhere.herokuapp.com/https://interfaz.cenart.gob.mx/api/video/catalogo/?id_post=28958',
+    type:  'GET',
+    beforeSend: function () {
+            $(".resultado").html("Procesando, espere por favor...");
+    },
+    success:  function (response) {
+      data = response[0];
+     console.log(response[0].video_videoclip);
+      $(".resultado").css({'display':'none'});  
+      data.video_videoclip.forEach(function(video, index) {
+        $(".for").append('<tr class="col-4"><td><img class="img_video" src="https://storage.googleapis.com/stateless-interfaz-cenart-gob/2018/06/01_intro.jpg"><p class="title_video">'+video.titulo_video+'</p><p class="desc_video">Cortinilla de entrada a la colección 35 años CITRU.</p></td></tr>');
+      });
+      $('.tituloM').html(this.productos[id].titulo);
+      $('.descripcionM').html(this.productos[id].descripcion);
+      $('.precioM').html(this.productos[id].precio);
+      $('.imgdetalle').attr('src', this.productos[id].img);          
+    }
+  });
+});*/   
+
+
+
